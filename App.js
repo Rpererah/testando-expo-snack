236aa85a255b6ec28876a74a4dec31db2fc73c10
rpererah <rpererah@gmail.com>
2022-08-20 11:27:52 -0300

    testando-expo-snack

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <View>
+        <Text style={{textAlign:'center'}}>Login</Text>
+        <Text>Email:</Text>
+        <TextInput
+        placeholder='email@exemplo.com'
+        />
+        <Text>Senha:</Text>
+        <TextInput
+        secureTextEntry
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
